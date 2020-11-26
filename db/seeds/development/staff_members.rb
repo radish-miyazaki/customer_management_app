10f4ad3dfa9c2c71d9280c6bf758a0c97124ecb4
radish-miyazaki <y.hidaka.kobe@gmail.com>
2020-11-26 21:06:27 +0900

    管理者が職員を管理するためのコントローラstaff_members controllerの作成 &　職員一覧画面の作成

--- db/seeds/development/staff_members.rb
+++ db/seeds/development/staff_members.rb
@@ -7,3 +7,35 @@ StaffMember.create!(
   password:         "password",
   start_date:       Date.today
 )
+
+family_names = %w{
+  佐藤:サトウ:sato
+  鈴木:スズキ:suzuki
+  高橋:タカハシ:takahashi
+  田中:タナカ:tanaka
+}
+
+given_names = %w{
+  二郎:ジロウ:jiro
+  三郎:サブロウ:saburo
+  四郎:シロウ:shiro
+  五郎:ゴロウ:goro
+  六郎:ロクロウ:rokuro
+}
+
+20.times do |n|
+  fn = family_names[n % 4].split(":")
+  gn = given_names[n % 5].split(":")
+
+  StaffMember::create!(
+    email: "#{fn[2]}.#{gn[2]}@example.com",
+    family_name: fn[0],
+    given_name: gn[0],
+    family_name_kana: fn[1],
+    given_name_kana:  gn[1],
+    password:         "password",
+    start_date:       (100 - n).days.ago.to_date,
+    end_date: n == 0 ? Date.today : nil,  # n=0のときだけ今日の日付にする
+    suspected: n == 1                     # n=1のときだけtureにする
+  )
+end
